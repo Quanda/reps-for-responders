5cc0944b0eb53c2d03ff818fcaa88f7889335c7b
Eric Furspan <eric.furspan@gmail.com>
2020-06-20 18:44:09 -0400

    remove query from page wrapper

--- src/helpers/pageWrapper.jsx
+++ src/helpers/pageWrapper.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
 import { Head, Header } from '../components';
 
 const Page = ({ data, children }) => {
   const { name, caption } = data.strapiBusiness;
 
   return (
-    <div>
+    <>
       <Head />
       <Header title={name} caption={caption} />
       {children}
-    </div>
+    </>
   );
 };
 
@@ -19,22 +18,4 @@ Page.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-const PageWithQuery = (props) => (
-  <StaticQuery
-    query={graphql`
-      query($strapiBusinessId: String) {
-        strapiBusiness(id: { eq: $strapiBusinessId }) {
-          name
-          caption
-        }
-      }
-    `}
-    render={(data) => <Page data={data}>{props.children}</Page>}
-  />
-);
-
-PageWithQuery.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default PageWithQuery;
+export default Page;
